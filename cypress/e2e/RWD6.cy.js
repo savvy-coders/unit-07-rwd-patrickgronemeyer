e3a56d0a47b0e93dc4e2b6a443c80a62e2400d4e
David Rodriguez <davidrodrigueznolimit@gmail.com>
2024-01-31 13:54:40 -0600

    update to remove cypress

--- cypress/e2e/RWD6.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Media Query Tests', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-  
-    it('should have responsive layout for navigation menu items', () => {
-      cy.get('.nav').should('have.css', 'display', 'flex');
-    });
-});
